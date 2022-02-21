d52ba7bbb51e6991cabdca90508a0e132f23f8a8
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-02-21 20:44:37 -0300

    Projeto Criado

---
+++ src/main/java/com/gabrielferreira/br/DemoSpringBootCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootCalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielferreira/br/DemoSpringBootCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
